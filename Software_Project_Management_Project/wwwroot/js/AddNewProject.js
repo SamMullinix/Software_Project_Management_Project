2c96826d8501f571c94c08c9f531fb0330c17e84
Sam Mullinix <sam.mullinix@gmail.com>
2018-11-10 10:49:48 -0500

    Added Requirmnts New Item Input Group

--- Software_Project_Management_Project/wwwroot/js/AddNewProject.js
+++ Software_Project_Management_Project/wwwroot/js/AddNewProject.js
@@ -1,4 +1,10 @@
-﻿
+﻿var rowNumber = 6;
+
 function myFunction() {
-    console.log("test");
+    var table = document.getElementById("mytable");
+    var row = table.insertRow(rowNumber);
+    rowNumber++;
+    var projectName = row.insertCell(0);
+    projectName.style = "font-size: 150%";
+    projectName.innerHTML = 
 }
