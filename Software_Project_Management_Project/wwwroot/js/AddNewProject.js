a597174f616b2107d40e3ff533b65f91e5d21152
Sam Mullinix <sam.mullinix@gmail.com>
2018-11-10 11:58:37 -0500

    Add New Row V1

--- Software_Project_Management_Project/wwwroot/js/AddNewProject.js
+++ Software_Project_Management_Project/wwwroot/js/AddNewProject.js
@@ -1,10 +1,26 @@
-﻿var rowNumber = 6;
-
-function myFunction() {
+﻿function myFunction() {
     var table = document.getElementById("mytable");
-    var row = table.insertRow(rowNumber);
-    rowNumber++;
+    var row = table.insertRow(1);
+
+    // Cell 1
     var projectName = row.insertCell(0);
-    projectName.style = "font-size: 150%";
-    projectName.innerHTML = 
+    var strong = document.createElement("strong");
+    strong.innerHTML = document.getElementById("newProjectForm").value;
+    projectName.appendChild(p);
+
+    // Cell 2
+    var reqButton = row.insert(1);
+
 }
+
+/* 
+<td><p><strong>ProjectName 3C</strong></p></td>
+                            <td><button type="button" class="btn btn-primary">View REQ
+                                    <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                                </button></td>
+                            <td><button type="button" class="btn btn-info">View Details
+                                    <span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span>
+                                </button></td>
+                            <td><button type="button" class="btn btn-danger">
+                                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                </button></td> */
