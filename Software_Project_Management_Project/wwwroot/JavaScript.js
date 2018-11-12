30c873cc19a7807b450735554cd019028cb3b759
deeppatel77 <codyz77@yahoo.com>
2018-11-11 19:26:23 -0500

    Update Software_Project_Management_Project/wwwroot/JavaScript.js

--- Software_Project_Management_Project/wwwroot/JavaScript.js
+++ Software_Project_Management_Project/wwwroot/JavaScript.js
@@ -1,10 +1,10 @@
-﻿var throwConnectBox = function () {
-    chat_box = document.getElementById('box');
-    div = window.parent.document.createElement('div');
-    input = window.parent.document.createElement('input');
-    input.type = "submit";
-    input.value = "Join chat";
-    input.onclick = function () { conn.send('$connect\r\n'); };
-    div.appendChild(input);
-    chat_box.appendChild(div);
-}
+﻿var  = ["Req 1", "Req 2", "Req 3", "Req 4", "Req 5"]
+var str = '<ul>'
+
+slides.forEach(function (slide) {
+    str += '<li>' + slide + '</li>';
+});
+
+str += '</ul>';
+document.getElementById("slideContainer").innerHTML = str;
+<div id="slideContainer"></div>
