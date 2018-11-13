2529d5b491ad0365b182b336ecdfae92c7add321
Sam Mullinix <sam.mullinix@gmail.com>
2018-11-12 21:15:22 -0500

    Can Add New Projects & Delete them

--- Software_Project_Management_Project/wwwroot/js/AddNewProject.js
+++ Software_Project_Management_Project/wwwroot/js/AddNewProject.js
@@ -3,24 +3,23 @@
     var row = table.insertRow(1);
 
     // Cell 1
-    var projectName = row.insertCell(0);
+    var projectName = row.insertCell(-1);
     var strong = document.createElement("strong");
     strong.innerHTML = document.getElementById("newProjectForm").value;
     projectName.appendChild(strong);
 
-    // Cell 2
-    //var reqButton = row.insertCell(1);
 
+    var reqButton = row.insertCell(-1);
+    reqButton.innerHTML = "<button type='button' class='btn btn-primary'>View REQ <span class='glyphicon glyphicon-list-alt' aria-hidden='true'></span></button>";
+
+    var detailsButton = row.insertCell(-1);
+    detailsButton.innerHTML = "<button type='button' class='btn btn-info'>View Details<span class='glyphicon glyphicon-paperclip' aria-hidden='true'></span></button>";
+
+    var trashButton = row.insertCell(-1);
+    trashButton.innerHTML = "<button type='button' class='btn btn-danger' onclick='DeleteRowFunction(this)'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button>";
 }
 
-/* 
-<td><p><strong>ProjectName 3C</strong></p></td>
-                            <td><button type="button" class="btn btn-primary">View REQ
-                                    <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                                </button></td>
-                            <td><button type="button" class="btn btn-info">View Details
-                                    <span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span>
-                                </button></td>
-                            <td><button type="button" class="btn btn-danger">
-                                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                                </button></td> */
+function DeleteRowFunction(obj) {
+     var p=obj.parentNode.parentNode;
+         p.parentNode.removeChild(p);
+    }
